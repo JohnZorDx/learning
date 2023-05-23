bd2a46a78544c263036fa813f328f0df278984b6
Jhonathan <Jhonathan.Engineer@gmail.com>
2023-05-23 15:22:35 -0300

    operadores aritmeticos

---
+++ Ada-Tech/Javascript/operadores_Aritmeticos.js
@@ -0,0 +1,36 @@
+//Operadores Aritmeticos
+
+let primeiroNumero = 20
+let segundoNumero = 2
+
+// adição: +
+// subtração: -
+// Multiplicação: *
+// Divisão: /
+// Exponenciação: **
+// Resto da Divisão: %
+
+const soma = primeiroNumero + segundoNumero
+const subtracao = primeiroNumero - segundoNumero
+const multiplicacao = primeiroNumero * segundoNumero
+const Divisao = primeiroNumero / segundoNumero
+const exponenciacao = primeiroNumero ** segundoNumero
+const restoDaDivisao = primeiroNumero % segundoNumero
+
+
+console.log('Soma =', soma)
+console.log('Subtraçao =', subtracao)
+console.log('Multiplicaçao =', multiplicacao)
+console.log('Divisao =', Divisao)
+console.log('Exponenciaçao =', exponenciacao)
+console.log('Resto da Divisao =', restoDaDivisao)
+
+// Precedencia de Operadores
+
+console.log((3 + 7) * 3)
+console.log((10 + 7 + 8) / 3)
+
+// Biblioteca Math
+
+console.log(Math.PI) // valor de P.I
+console.log(Math.sqrt(16)) // raiz quadrada
