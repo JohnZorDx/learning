9d0d6583fa029009e85d5c116d6899dd693e4d61
Jhonathan <Jhonathan.Engineer@gmail.com>
2023-05-23 14:25:24 -0300

    add: variaveis

---
+++ Ada-Tech/Javascript/variaveis.js
@@ -0,0 +1,21 @@
+// Variaveis        
+
+let idade = 26 //Criando a primeira váriavel.
+
+console.log(idade)
+
+//tipos de váriaveis
+
+idade = 30                    //Number
+let numero = 30               //Number
+let altura = 1.90             //Number
+let nome = 'Jhonathan'        //String (texto)
+let estudando = true          //Boleano (True or False)
+
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+const constante = 100
