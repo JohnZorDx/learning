ecc830881c76831a8b81d855735cb8e1481f3901
Jhonathan <Jhonathan.Engineer@gmail.com>
2023-05-26 05:29:46 -0300

    add: condicionais

---
+++ Ada-Tech/Javascript/condicionais.js
@@ -0,0 +1,23 @@
+// ESTRUTURAS CONDICIONAIS
+
+const idade = 18
+
+if (idade >= 18) {
+    console.log('Voce é maior de idade')
+} else {
+    console.log('voce é menor de idade')
+}
+
+console.clear()
+
+// MEDIA ESCOLAR
+
+let media = 4
+
+if (media >= 7){
+    console.log('Aprovado')
+} else if (media >= 5){
+    console.log('Recuperaçao')
+} else {
+    console.log('Reprovado')
+}
