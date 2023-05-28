937e20a02307cfbe6da24f56f066859821500e99
Jhonathan <Jhonathan.Engineer@gmail.com>
2023-05-27 22:23:36 -0300

    react parte 2

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,9 +1,32 @@
+import MeuComponente from "./components/MeuComponente"
+
 function App() {
   
 
   return (
-   <h1>Olá Mundo, React</h1>
+    <div>
+      <h1>Olá Mundo, React</h1>
+      <MeuComponente />
+      <MeuBotao conteudo='me clique'/> 
+
+      <MeuBotao conteudo='depois aqui'/>
+
+      <MeuBotao conteudo='e por fim aqui'/>
+    
+   </div>
   )
 }
+
+
+function MeuBotao(props) {
+
+  console.log()
+  return (
+    <button>{props.conteudo}</button>
+  )
+}
+
+
+
 
 export default App
---
+++ meu-projeto-react/src/components/MeuComponente.jsx
@@ -0,0 +1,9 @@
+export default function MeuComponente() {
+    return (
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Consectetur a placeat ut voluptatem veniam inventore ea nam deserunt ratione, 
+      ex temporibus recusandae in blanditiis magnam, quibusdam beatae numquam, alias iure.</p>
+      )
+  }
+
+  
