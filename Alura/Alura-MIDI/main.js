6278f3dc483d4a13ee67cd182994c8c9b42f35d5
Jhonathan Henrique <Jhonathan.engineer@gmail.com>
2023-06-16 18:10:37 -0300

    listas de elementos

---
+++ Alura/Alura-MIDI/main.js
@@ -0,0 +1,15 @@
+// toca som pom
+
+// function tocaSomPom(){
+//     document.querySelector("#som_tecla_pom").play()
+// }
+// document.querySelector(".tecla_pom").onclick = tocaSomPom;
+
+//trabalhando com listas
+
+function tocaSomPom(){
+    document.querySelector("#som_tecla_pom").play()
+}
+
+documento.querySelectorAll(".tecla").onclick = tocaSomPom
+
